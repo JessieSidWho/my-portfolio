8f9befead22c254b7c977349e37e78de0690f784
Jessie Sidhu <jsidhu22@gmail.com>
2019-07-07 19:42:21 -0700

    deploy

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82f27933eaf9d9c66a91a39d046d9fd1.js"
+  "/precache-manifest.0b4e2c383ec0219b67082127e55307c4.js"
 );
 
 self.addEventListener('message', (event) => {
